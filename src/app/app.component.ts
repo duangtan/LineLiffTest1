c5e641574fb5c074e223d0b51f3422024c06ef14
duangtan <duangta_n@silpakorn.edu>
2024-05-23 20:52:22 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Line_Liff';
+export class AppComponent implements OnInit {
+  title = 'my-liff-app';
+
+  ngOnInit() {
+    this.initializeLiff();
+  }
+
+  initializeLiff() {
+    liff.init({ liffId: 'YOUR_LIFF_ID' })
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          this.getProfile();
+        } else {
+          liff.login();
+        }
+      })
+      .catch((err: any) => {
+        console.error('LIFF Initialization failed', err);
+      });
+  }
+
+  getProfile() {
+    liff.getProfile()
+      .then(profile => {
+        console.log(profile);
+        // เพิ่มโค้ดเพื่อแสดงโปรไฟล์หรือใช้ข้อมูลโปรไฟล์ที่ได้ตามต้องการ
+      })
+      .catch((err: any) => {
+        console.error('Error getting profile', err);
+      });
+  }
 }
